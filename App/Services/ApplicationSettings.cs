ac75391cec51b0c8fca26e55bfd89370a1c82333
Ali Torabi <AliTorabi2020@Gmail.com>
2023-09-02 09:41:20 +0200

    Improve missing image calculation

--- App/Services/ApplicationSettings.cs
+++ App/Services/ApplicationSettings.cs
@@ -1,7 +1,10 @@
 ﻿namespace App.Services;
+
 public record ApplicationSettings
 {
     public bool RunOnStartup { get; set; }
+
+    public int InitialLoadingImageCount { get; set; }
 
     public string BasePath => $"{Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "BingWallpaper")}";
 }
--- Wpf/ViewModels/MainViewModel.cs
+++ Wpf/ViewModels/MainViewModel.cs
@@ -8,123 +8,124 @@ using System.Windows;
 using Wpf.Helpers;
 using Wpf.Utility;
 
-namespace Wpf.ViewModels
+namespace Wpf.ViewModels;
+
+public class MainViewModel : BaseViewModel
 {
-    public class MainViewModel : BaseViewModel
+    private const int BING_IMAGE_AVAILABLE_DAYS = 7;
+
+    private readonly ApplicationSettings _applicationSettings;
+    private readonly IImageService _imageService;
+
+    public static SnackbarMessageQueue MessageQueue { get; private set; }
+
+    public RelayCommand SetWallpaperCommand { get; set; }
+
+    public RelayCommand SettingCommand { get; set; }
+
+    public RelayCommand AboutCommand { get; set; }
+
+    public RelayCommand CloseCommand { get; set; }
+
+    public ObservableCollection<ImageInfo> Images { get; set; }
+
+    private ImageInfo _selectedImage;
+    public ImageInfo SelectedImage { get => _selectedImage; set { _selectedImage = value; RaisePropertyChanged(nameof(SelectedImage)); } }
+
+    public MainViewModel()
     {
-        private const int IMAGE_COUNT = 7;
+        _applicationSettings = App.ServiceProvider.GetRequiredService<ApplicationSettings>();
+        _imageService = App.ServiceProvider.GetRequiredService<IImageService>();
+        Images = new ObservableCollection<ImageInfo>();
+        MessageQueue = new SnackbarMessageQueue();
 
-        private readonly IImageService _imageService;
+        SetWallpaperCommand = new RelayCommand(SetWallpaper);
+        SettingCommand = new RelayCommand(Setting);
+        AboutCommand = new RelayCommand(About);
+        CloseCommand = new RelayCommand(Close);
 
-        public static SnackbarMessageQueue MessageQueue { get; private set; }
+        Initialize();
+    }
 
-        public RelayCommand SetWallpaperCommand { get; set; }
+    public async void Initialize()
+    {
+        await Task.Run(LoadImagesAsync);
 
-        public RelayCommand SettingCommand { get; set; }
+        await Task.Run(DownloadImagesAsync);
+    }
 
-        public RelayCommand AboutCommand { get; set; }
+    private void SetWallpaper(object parameter)
+    {
+        MessageQueue.Enqueue("Trying to Set Wallpaper.");
 
-        public RelayCommand CloseCommand { get; set; }
+        NativeMethods.SetWallpaper(SelectedImage.Name);
+    }
 
-        public ObservableCollection<ImageInfo> Images { get; set; }
+    private void Setting(object parameter)
+    {
+        MessageQueue.Enqueue("Please edit this file and restart the app");
 
-        private ImageInfo _selectedImage;
-        public ImageInfo SelectedImage { get => _selectedImage; set { _selectedImage = value; RaisePropertyChanged(nameof(SelectedImage)); } }
-
-        public MainViewModel()
+        Process.Start(new ProcessStartInfo
         {
-            _imageService = App.ServiceProvider.GetRequiredService<IImageService>();
-            Images = new ObservableCollection<ImageInfo>();
-            MessageQueue = new SnackbarMessageQueue();
+            FileName = "appsettings.json",
+            UseShellExecute = true
+        });
+    }
 
-            SetWallpaperCommand = new RelayCommand(SetWallpaper);
-            SettingCommand = new RelayCommand(Setting);
-            AboutCommand = new RelayCommand(About);
-            CloseCommand = new RelayCommand(Close);
-
-            Initialize();
-        }
-
-        public async void Initialize()
+    private void About(object parameter)
+    {
+        if (SelectedImage is not null)
         {
-            await Task.Run(LoadImagesAsync);
-
-            await Task.Run(DownloadImagesAsync);
-        }
-
-        private void SetWallpaper(object parameter)
-        {
-            MessageQueue.Enqueue("Trying to Set Wallpaper.");
-
-            NativeMethods.SetWallpaper(SelectedImage.Name);
-        }
-
-        private void Setting(object parameter)
-        {
-            MessageQueue.Enqueue("Please edit this file and restart the app");
-
             Process.Start(new ProcessStartInfo
             {
-                FileName = "appsettings.json",
+                FileName = $"https://bing.com{SelectedImage.Url}",
                 UseShellExecute = true
             });
         }
+    }
 
-        private void About(object parameter)
+    private void Close(object parameter)
+    {
+        CloseWindow();
+    }
+
+    private async Task LoadImagesAsync()
+    {
+        MessageQueue.Enqueue("Loading Wallpapers ...");
+
+        var images = await _imageService.GetImagesAsync(pageSize: _applicationSettings.InitialLoadingImageCount);
+        foreach (var image in images)
         {
-            if (SelectedImage is not null)
+            Application.Current.Dispatcher.Invoke(delegate
             {
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = $"https://bing.com{SelectedImage.Url}",
-                    UseShellExecute = true
-                });
-            }
+                Images.Add(image);
+            });
         }
 
-        private void Close(object parameter)
+        if (images.Count > 0)
         {
-            CloseWindow();
+            Images = new ObservableCollection<ImageInfo>(Images.OrderByDescending(p => p.CreatedOn));
+            RaisePropertyChanged(nameof(Images));
+            SelectedImage = Images.FirstOrDefault();
         }
 
-        private async Task LoadImagesAsync()
+        MessageQueue.Clear();
+    }
+
+    private async Task DownloadImagesAsync()
+    {
+        MessageQueue.Enqueue("Downloading New Wallpapers ...");
+
+        var days = SelectedImage is null
+            ? BING_IMAGE_AVAILABLE_DAYS
+            : Math.Min((DateTime.Today - SelectedImage.CreatedOn).Days, BING_IMAGE_AVAILABLE_DAYS);
+
+        if (days > 0)
         {
-            MessageQueue.Enqueue("Loading Wallpapers ...");
-
-            var images = await _imageService.GetImagesAsync(pageSize: IMAGE_COUNT);
-            foreach (var image in images)
-            {
-                Application.Current.Dispatcher.Invoke(delegate
-                {
-                    Images.Add(image);
-                });
-            }
-
-            if (images.Count > 0)
-            {
-                Images = new ObservableCollection<ImageInfo>(Images.OrderByDescending(p => p.CreatedOn));
-                RaisePropertyChanged(nameof(Images));
-                SelectedImage = Images.FirstOrDefault();
-            }
-
-            MessageQueue.Clear();
+            await _imageService.DownloadImagesAsync(days);
+            await LoadImagesAsync();
         }
 
-        private async Task DownloadImagesAsync()
-        {
-            MessageQueue.Enqueue("Downloading New Wallpapers ...");
-
-            var lastImageDays = SelectedImage is null ? 0 : (DateTime.Today - SelectedImage.CreatedOn).Days;
-            var bingImageAvailability = IMAGE_COUNT - Images.Count;
-
-            var days = Math.Max(lastImageDays, bingImageAvailability);
-            if (days > 0)
-            {
-                await _imageService.DownloadImagesAsync(days);
-                await LoadImagesAsync();
-            }
-
-            MessageQueue.Clear();
-        }
+        MessageQueue.Clear();
     }
 }
