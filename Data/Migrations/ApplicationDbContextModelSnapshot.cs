df7fd317b5b8c74c760c079e614c19ca6cf62304
Ali Torabi <AliTorabi2020@Gmail.com>
2023-09-09 10:25:36 +0200

    Move SQLite library to upper layer

--- Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -12,7 +12,8 @@ partial class ApplicationDbContextModelSnapshot : ModelSnapshot
 {
     protected override void BuildModel(ModelBuilder modelBuilder)
     {
-#pragma warning disable 612, 618
+        #pragma warning disable 612, 618
+
         modelBuilder.HasAnnotation("ProductVersion", "7.0.10");
 
         modelBuilder.Entity("Data.Models.ImageInfo", b =>
@@ -37,6 +38,7 @@ partial class ApplicationDbContextModelSnapshot : ModelSnapshot
 
                 b.ToTable("Images");
             });
-#pragma warning restore 612, 618
+
+        #pragma warning restore 612, 618
     }
 }
