18b65f856c4c41155cd1f9b1f62d1d8cb30e96c9
Ali Torabi <AliTorabi2020@Gmail.com>
2023-09-25 14:50:11 +0200

    Update RelayCommand.cs

--- Wpf/Helpers/RelayCommand.cs
+++ Wpf/Helpers/RelayCommand.cs
@@ -16,19 +16,13 @@ public class RelayCommand : ICommand
         _canExecute = canExecute;
     }
 
-    public bool CanExecute(object parameter)
-    {
-        return _canExecute is null || _canExecute(parameter);
-    }
-
     public event EventHandler CanExecuteChanged
     {
         add { CommandManager.RequerySuggested += value; }
         remove { CommandManager.RequerySuggested -= value; }
     }
 
-    public void Execute(object parameter)
-    {
-        _execute(parameter);
-    }
+    public bool CanExecute(object parameter) => _canExecute is null || _canExecute(parameter);
+
+    public void Execute(object parameter) => _execute(parameter);
 }
